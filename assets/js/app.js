23757a351e4e56f56cfb55368f2986fff9f208da
Anton Yermakovich <antonyermakovich@gmail.com>
2025-03-14 18:48:00 +0300

    add send message function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,10 @@
+const header = document.querySelector(".header");
 const headerContainer = document.querySelector(".header__container");
+const menuMobile = document.querySelector(".menu__mobile");
+const linkMenuMobile = document.querySelectorAll(".menu__mobile li a");
+const body = document.querySelector("body");
+const overlay = document.querySelector(".overlay");
+const modalThx = document.querySelector(".modal");
 
 window.addEventListener("scroll", () => {
   if (window.pageYOffset > 20) {
@@ -9,12 +15,83 @@ window.addEventListener("scroll", () => {
 });
 
 const burger = document.querySelector(".header__icon");
-burger.addEventListener('click', openMenu)
+burger.addEventListener("click", openMenu);
 
 function openMenu() {
-  if(this.classList.contains('header__icon_active')) {
-    this.classList.remove('header__icon_active')
+  if (this.classList.contains("header__icon_active")) {
+    closeMenu();
   } else {
-    this.classList.add('header__icon_active')
+    burger.classList.add("header__icon_active");
+    header.classList.add("header__open");
+    headerContainer.classList.add("header__container_border-radius");
+    menuMobile.classList.add("menu__mobile_show");
+    body.classList.add("body_hidden");
+    overlay.classList.add("overlay_show");
   }
 }
+
+linkMenuMobile.forEach((link) => link.addEventListener("click", closeMenu));
+overlay.addEventListener("click", closeMenu);
+
+function closeMenu() {
+  burger.classList.remove("header__icon_active");
+  header.classList.remove("header__open");
+  headerContainer.classList.remove("header__container_border-radius");
+  menuMobile.classList.remove("menu__mobile_show");
+  body.classList.remove("body_hidden");
+  overlay.classList.remove("overlay_show");
+}
+
+// validate form
+const validator = new JustValidate("#form");
+validator
+  .addField("#name", [
+    {
+      rule: "required",
+      errorMessage: "Введите ваше имя",
+    },
+    {
+      rule: "minLength",
+      value: 2,
+      errorMessage: "Минимальная длина имени 2 символа",
+    },
+  ])
+  .addField("#telegram", [
+    {
+      rule: "required",
+      errorMessage: "Введите ваш телеграм",
+    },
+  ]);
+
+// Send message
+document.querySelector("#form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  if (validator.isValid) {
+    const formData = {
+      name: e.target.name.value,
+      telegram: e.target.telegram.value,
+    };
+
+    await fetch("mail.php", {
+      method: "POST",
+      body: JSON.stringify(formData),
+    });
+
+    modalThx.classList.add("modal_show");
+    modalThx.addEventListener("click", (e) => {
+      if (
+        e.target.classList.contains("modal__overlay") ||
+        e.target.tagName === "BUTTON"
+      ) {
+        modalThx.classList.remove("modal_show");
+      }
+    });
+    e.target.reset();
+  }
+});
+
+AOS.init({
+  offset: 120,
+  once: true,
+});
